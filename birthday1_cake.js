d8b00616028f3dc3219c9a1dc5c4c53aa8a599f7
Pavel Saikov <pavsaikov@gmail.com>
2020-02-13 13:59:30 +0300

    feat: make task  'Birthday 1 - cake'

---
+++ birthday1_cake.js
@@ -0,0 +1,19 @@
+function cake(x, y){
+    let sum = 0;
+
+    for(let i = 0; i < y.length; i++) {
+        if(i % 2 == 0) {
+            sum += y.charCodeAt(i);
+        } else {
+            sum += y.charCodeAt(i) - 96;
+        }
+    }
+
+    if(Math.round(x * 0.7) < sum) return "Fire!";
+    return "That was close!";
+}
+
+
+alert(cake(900, 'abcdef'));
+alert(cake(56, 'ifkhchlhfd'));
+alert(cake(256, 'aaaaaddddr'));
